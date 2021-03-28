cb11bf7cbfed6a583ef0c00583b01d557a058cad
Gaspar Dolcemascolo <gaspardolcemascolo@gmail.com>
2021-03-28 09:05:26 -0300

    home

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -4,12 +4,41 @@ import Carousel from '../../components/Carousel'
 import CarouselItem from '../../components/CarouselItem'
 import Header from '../../components/Header'
 
+import './Home.css'
+
 const Home = () => {
 	return(
 		<>
 			<Header />
 			<main className="Home">
-				<h2>Originals</h2>
+				<section className="Home__mainVideo">
+					<video className="Home__mainVideo-video">
+						<source src="#"></source>
+					</video>
+				</section>
+				<h2 className="Home__category">Originals</h2>
+				<Carousel >
+					<CarouselItem />
+					<CarouselItem />
+					<CarouselItem />
+					<CarouselItem />
+					<CarouselItem />
+					<CarouselItem />
+					<CarouselItem />
+					<CarouselItem />
+				</Carousel>
+				<h2 className="Home__category">My List</h2>
+				<Carousel >
+					<CarouselItem />
+					<CarouselItem />
+					<CarouselItem />
+					<CarouselItem />
+					<CarouselItem />
+					<CarouselItem />
+					<CarouselItem />
+					<CarouselItem />
+				</Carousel>
+				<h2 className="Home__category">Trends</h2>
 				<Carousel >
 					<CarouselItem />
 					<CarouselItem />
